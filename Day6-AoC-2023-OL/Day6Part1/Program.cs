611d3030b25db61a67eee02f6d0a8c2090abdf0a
javillo-ur <55446633+javillo-ur@users.noreply.github.com>
2023-12-09 16:42:46 +0100

    Upload files

---
+++ Day6-AoC-2023-OL/Day6Part1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(new List<string[]>(){File.ReadAllLines("input.txt")}.Select(j =>
+    new Tuple<List<int>,List<int>>(j[0].Split(" ").Where(r => int.TryParse(r, out _)).Select(r => int.Parse(r)).ToList(), j[1].Split(" ").Where(r => int.TryParse(r, out _)).Select(r => int.Parse(r)).ToList()))
+    .Select(t => t.Item1.Zip(t.Item2).ToList())
+    .Select(j => j.Aggregate(1, (previous, current) => previous * 
+        Enumerable.Range(0, current.First).Where(t => t*(current.First - t) > current.Second).Count())).First());
